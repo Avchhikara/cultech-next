2f155601984d9950da1a15fcb8369022367865a8
Avnish <Avnishway@gmail.com>
2020-03-10 04:37:11 +0530

    Modifying homepage as per the needs

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,27 +1,32 @@
 import { Row, Col, Card, CardBody, Button, CardTitle } from "reactstrap";
 import Router from "next/router";
 
-import TCoord from "./TeacherCoordinators";
-import SCoord from "./StudentCoordinators";
 import Rules from "./Rules";
 
 const Home = props => {
   return (
     <React.Fragment>
-      <Row>
+      <Row className="home-fullpage">
         <Col sm={10} className="m-auto">
           <h1 className="display-4 text-success text-center">CulTech 2020</h1>
           <div className="lead text-center">
-            Cultural and Technical Festival of ECE Department
+            Come and witness the ir-resist-able
           </div>
         </Col>
       </Row>
-      <Row className="mt-4">
+      <Row className="homepage_morebutton">
+        <Col sm={10} className="m-auto text-center">
+          <a href="#more" className="m-auto">
+            &#x2193;
+          </a>
+        </Col>
+      </Row>
+      <Row className="mt-4" id="more">
         <Col sm={10} className="m-auto">
           <h4 className="text-success">What is it?</h4>
           <p>
             Cultech is the Cultural and Technical fest organised by INTEC
-            society anually. Intech is the official society represing ECE
+            society annually. INTEC is the official society representing ECE
             department in DCRUST Murthal.
           </p>
         </Col>
