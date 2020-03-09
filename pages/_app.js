0373e9958b09f62567b6bf547904d1e8aaba7acb
Avnish <Avnishway@gmail.com>
2020-03-09 13:56:30 +0530

    Adding loader

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,8 @@
+import App from "next/app";
+import NProgress from "nprogress";
+import Router from "next/router";
+import Head from "next/head";
+
 import Header from "./../components/Header/Header";
 import Footer from "./../components/Footer/Footer";
 
@@ -8,16 +13,35 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./../style.css";
 
-function App({ Component, pageProps }) {
-  return (
-    <div>
-      <Header />
-      <div className="container" style={{ minHeight: "70vh" }}>
-        <Component {...pageProps} />
-      </div>
-      <Footer />
-    </div>
-  );
+Router.events.on("routeChangeStart", url => {
+  NProgress.start();
+});
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
+class MyApp extends App {
+  render() {
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <Head>
+          {/* Import CSS for nprogress */}
+          <link rel="stylesheet" type="text/css" href="/nprogress.css" />
+          <title>CulTech - Cultural and Technical fest of ECE department</title>
+        </Head>
+        <style jsx>{`
+          a {
+            margin: 0 10px 0 0;
+          }
+        `}</style>
+        <Header />
+        <div className="container" style={{ minHeight: "70vh" }}>
+          <Component {...pageProps} />
+        </div>
+        <Footer />
+      </>
+    );
+  }
 }
 
 // App.getInitialProps = ctx => {
@@ -26,4 +50,4 @@ function App({ Component, pageProps }) {
 //   return { isLogged: !!out };
 // };
 
-export default App;
+export default MyApp;
